3845e85e6847bd8bdf6c7c213f7031d5b7749392
savstudios <brhmem2015@gmail.com>
2025-01-14 17:55:43 +0000

    small changes, learning shaders

--- src/main.cpp
+++ src/main.cpp
@@ -31,16 +31,18 @@ const int SCREEN_WIDTH = 600;
 
 const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
-
+"out vec4 vertexColor;\n"
 "void main(){\n"
-"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"   gl_Position = vec4(aPos, 1.0);\n"
+"   vertexColor = vec4(0.5, 0.0, 0.0, 1.0);\n"
 "}\0";
 
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"uniform vec4 vertexColor;\n"
 "void main()\n"
 "{\n"
-"FragColor = vec4(0.0f, 0.9f, 0.2f, 1.0f);\n"
+"FragColor = vertexColor;\n"
 "}\n";
 
 unsigned int VBO, VAO, EBO, shaderProgram;
@@ -141,6 +143,8 @@ void Render(){
 
 void HandleInput(GLFWwindow* window){
    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS){ glfwSetWindowShouldClose(window, true); }
+   if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS){ glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); }
+   if(glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_RELEASE){ glPolygonMode(GL_FRONT_AND_BACK, GL_FILL); }
 }
 
 void TerminateAll(){
